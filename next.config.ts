d51cef9b0b9ca06f29791f5adbd1c43bf1d4c6a5
sameerkhan86926-a11y <sameerkhan86926@gmail.com>
2026-09-23 15:16:18 +0530

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,12 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: "export",
+
+  basePath: "/hisabpro",
+  assetPrefix: "/hisabpro/",
+
+  trailingSlash: true,
+
   images: {
     unoptimized: true
   }
