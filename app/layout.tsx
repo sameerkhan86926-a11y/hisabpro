f4ed6510ed31f93224d3e58a69c27675dc1584b6
sameerkhan86926-a11y <sameerkhan86926@gmail.com>
2026-09-24 00:31:23 +0530

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import "./globals.css";
+import AppLock from "../components/AppLock";
 
 export const metadata: Metadata = {
   title: "HisabPro",
@@ -13,7 +14,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body>
+        <AppLock>{children}</AppLock>
+      </body>
     </html>
   );
 }
