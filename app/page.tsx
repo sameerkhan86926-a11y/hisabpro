4c4cec557e36d7a7deaabdb969215b3375689826
sameerkhan86926-a11y <sameerkhan86926@gmail.com>
2026-09-23 19:49:48 +0530

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -230,15 +230,18 @@ export default function Dashboard() {
           <p>Sales • Stock • Khata • Profit</p>
         </div>
 
-        <button className="notification">
-          <svg
-            viewBox="0 0 24 24"
-            aria-hidden="true"
-          >
-            <path d="M18 8a6 6 0 0 0-12 0c0 7-3 7-3 9h18c0-2-3-2-3-9" />
-            <path d="M10 21h4" />
-          </svg>
-        </button>
+        <a
+  href="/hisabpro/notifications/"
+  className="notification"
+>
+  <svg
+    viewBox="0 0 24 24"
+    aria-hidden="true"
+  >
+    <path d="M18 8a6 6 0 0 0-12 0c0 7-3 7-3 9h18c0-2-3-2-3-9" />
+    <path d="M10 21h4" />
+  </svg>
+</a>
       </header>
 
       {/* WELCOME */}
